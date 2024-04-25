f96a6ff1f107a465a88ab4547541532151056d68
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2024-04-25 15:59:21 +0200

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Hero from "./_components/Hero";
+export const metadata = {
+    title:"Nibras"
+
+
+
+};
+export default function Home() {
+  return (
+   <div>
+     <Hero />
+   </div>
+  );
+}
