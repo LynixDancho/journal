3978b3c10e3ffb98bfe714cfe4e2fd80fbd6f309
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2024-04-28 20:37:54 +0200

    yes

--- app/_utils/ArticleApi.js
+++ app/_utils/ArticleApi.js
@@ -1,7 +1,9 @@
 const { default: axiosClient } = require("./axiosClient");
 
 const getLatestArticles=()=>axiosClient.get('/articles?populate=*')
+const getArticleById=(id)=>axiosClient.get( `/articles/${id}?/populate=*`)
 export default{
 
-getLatestArticles
+getLatestArticles,
+getArticleById
 }
